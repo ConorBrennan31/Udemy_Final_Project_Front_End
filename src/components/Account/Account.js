a2f817802228d474d31bafaa78da59fcd22fe990
Conor Brennan <conorbrennan@Conors-MacBook-Pro.local>
2020-06-24 16:09:46 -0400

    fixing div

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Account = ({ name, emailAddress }) => {
 	return (
 		<div>
-			<p>Name</p>
-			<p>{name}</p>
-		</div>
-		<div>
-			<p>Email Address</p>
-			<p>{emailAddress}</p>
+			<div>
+				<p>Name</p>
+				<p>{name}</p>
+			</div>
+			<div>
+				<p>Email Address</p>
+				<p>{emailAddress}</p>
+			</div>
 		</div>
 
 	);
