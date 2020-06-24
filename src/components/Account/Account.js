9003e1c498b66c7e58aa6543dacba0741edf882d
Conor Brennan <conorbrennan@Conors-MacBook-Pro.local>
2020-06-24 16:54:00 -0400

    Adding update password functionality

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -5,10 +5,28 @@ class Account extends React.Component {
 	constructor(props) {
     super(props);
       this.state = {
-        signInEmail: '',
-        signInPassword: ''
+        updatedPassword: ''
       }
   }
+
+  	onPasswordChange = (event) => {
+		this.setState({updatedPassword: event.target.value});
+	}
+
+	onPasswordChangeSubmit = () => {
+		fetch('https://murmuring-crag-47327.herokuapp.com/profile', {
+			method: 'put',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({
+				email: emailAddress,
+				updatedPassword: this.state.updatedPassword 
+			})
+		})
+		.then(response => response.json())
+		.then(user => {
+			console.log(user)
+		})
+	}
 
   	render() {
   		const { name, emailAddress } = this.props;
@@ -23,7 +41,7 @@ class Account extends React.Component {
 					<h3>Email Address</h3>
 					<h5>{emailAddress}</h5>
 				</div>
-				<input placeholder="update password"></input>
+				<input placeholder="update password" onChange={this.onPasswordChange} type="password"></input>
 				<button>update</button>
 			</div>
 
