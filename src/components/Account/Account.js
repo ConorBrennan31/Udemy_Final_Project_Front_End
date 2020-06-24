1654924f7acd1db21396e28f6ccf9dcc0c94db4e
Conor Brennan <conorbrennan@Conors-MacBook-Pro.local>
2020-06-24 16:29:19 -0400

    changing formatting and adding update password

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -3,14 +3,17 @@ import React from 'react';
 const Account = ({ name, emailAddress }) => {
 	return (
 		<div>
+			<h1>Account</h1>
 			<div>
-				<p>Name</p>
-				<p>{name}</p>
+				<h3>Name</h3>
+				<h5>{name}</h5>
 			</div>
 			<div>
-				<p>Email Address</p>
-				<p>{emailAddress}</p>
+				<h3>Email Address</h3>
+				<h5>{emailAddress}</h5>
 			</div>
+			<input placeholder="update password"></input>
+			<button>update</button>
 		</div>
 
 	);
