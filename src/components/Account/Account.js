6180abbde356e0bafefee782ff86c3b3d8991426
Conor Brennan <conorbrennan@Conors-MacBook-Pro.local>
2020-06-24 16:37:44 -0400

    changing to smart component

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
-const Account = ({ name, emailAddress }) => {
-	return (
-		<div>
-			<h1>Account</h1>
-			<div>
-				<h3>Name</h3>
-				<h5>{name}</h5>
-			</div>
-			<div>
-				<h3>Email Address</h3>
-				<h5>{emailAddress}</h5>
-			</div>
-			<input placeholder="update password"></input>
-			<button>update</button>
-		</div>
+class Account extends React.Component {
 
-	);
+	constructor(props) {
+    super(props);
+      this.state = {
+        signInEmail: '',
+        signInPassword: ''
+      }
+  }
+
+  	render() {
+  		const { name, emailAddress } = this.props;
+		return (
+			<div>
+				<h1>Account</h1>
+				<div>
+					<h3>Name</h3>
+					<h5>{name}</h5>
+				</div>
+				<div>
+					<h3>Email Address</h3>
+					<h5>{emailAddress}</h5>
+				</div>
+				<input placeholder="update password"></input>
+				<button>update</button>
+			</div>
+
+		);
+	}
 }
 
 export default Account;
